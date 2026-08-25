2cfa926b484f1b61c9f23fcf22885884d4f7824c
Tom Bland <tom_bland@hotmail.co.uk>
2026-08-25 11:40:55 +0100

    No longer copy input data

--- muse2_data_analysis/helpers.py
+++ muse2_data_analysis/helpers.py
@@ -6,20 +6,7 @@ from . import run_muse2
 
 DATA_DIR = Path(__file__).parent.parent.absolute() / "data"
 EXAMPLE_NAME = "muse1_default"
-_INPUT_DIR = DATA_DIR / f"{EXAMPLE_NAME}_input"
-_OUTPUT_DIR = DATA_DIR / f"{EXAMPLE_NAME}_output"
-
-
-def get_example_input_dir() -> Path:
-    """Get the directory for example input data.
-
-    If the folder does not exist, we create it by extracting an example from muse2.
-    """
-    if _INPUT_DIR.exists():
-        return _INPUT_DIR
-
-    run_muse2("example", "extract", EXAMPLE_NAME, str(_INPUT_DIR))
-    return _INPUT_DIR
+_OUTPUT_DIR = DATA_DIR / f"{EXAMPLE_NAME}"
 
 
 def get_example_output_dir() -> Path:
@@ -30,9 +17,6 @@ def get_example_output_dir() -> Path:
     if _OUTPUT_DIR.exists():
         return _OUTPUT_DIR
 
-    # Ensure INPUT_DIR exists
-    get_example_input_dir()
-
     # Run the example model
-    run_muse2("run", str(_INPUT_DIR), "--output-dir", str(_OUTPUT_DIR))
+    run_muse2("example", "run", EXAMPLE_NAME, "--output-dir", str(_OUTPUT_DIR))
     return _OUTPUT_DIR
